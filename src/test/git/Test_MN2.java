535bf643e6f416707e33c5a64a641c3480bd1ae0
Mathilde Nicolas <nicolasm@3il.fr>
2020-04-20 11:05:56 +0200

    99, 111

--- src/test/git/Test_MN2.java
+++ src/test/git/Test_MN2.java
@@ -11,9 +11,9 @@ public class Test_MN2 {
 	
 	private String[] adresses = {
 
-			"14 Rue Mozart, Paris",
+			"99 Rue Mozart, Paris",
 			"77 Rue Picasso, Toulouse", 
-			"53 Rue des fleurs, Limoges",
+			"111 Rue des fleurs, Limoges",
 			};
 
 	
