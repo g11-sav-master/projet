f32d7d21e9459847bb717037eaa90c5e70d8f064
Vivien Maugarnis <maugarnv@3il.fr>
2020-05-15 12:04:52 +0200

    Correction de bug de merge

--- src/projet/commun/IMapper.java
+++ src/projet/commun/IMapper.java
@@ -5,11 +5,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
 import projet.data.Benevole;
+import projet.data.Participant;
 
 
 @Mapper
 public interface IMapper {
 	
 	Benevole update(@MappingTarget Benevole benevole, Benevole source);
+	Participant update(@MappingTarget Participant participant, Participant source);
 	
 }
