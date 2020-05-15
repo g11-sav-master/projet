d63d68db9444f4ac3bb2c129f9baccda4fc5aaa0
Romain V. <vilayler@3il.fr>
2020-05-15 12:09:28 +0200

    correction head

--- src/projet/view/test/ControllerTestDaoActionBenevole.java
+++ src/projet/view/test/ControllerTestDaoActionBenevole.java
@@ -22,11 +22,7 @@ public class ControllerTestDaoActionBenevole {
 	@Inject
 	private DaoActionBenevole	dao;
 	
-<<<<<<< HEAD
 	private final int		id = 1;	
-=======
-	private final int		id = 10;	
->>>>>>> branch 'master' of https://github.com/g11-sav-master/projet.git
 	
 	
 	// Actions
