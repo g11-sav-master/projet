0022aa599070698fcfbfba180af4be654105a29d
Romain V. <vilayler@3il.fr>
2020-05-25 00:01:47 +0200

    correctif prix raid

--- src/projet/view/raid/ControllerRaidForm.java
+++ src/projet/view/raid/ControllerRaidForm.java
@@ -1,11 +1,17 @@
 package projet.view.raid;
 
+import java.time.LocalTime;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
+
 import javax.inject.Inject;
 
 
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.util.StringConverter;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 import jfox.javafx.util.ConverterStringLocalDate;
@@ -38,6 +44,35 @@ public class ControllerRaidForm {
 	@Inject
 	private ModelRaid modelraid;
 
+	Pattern pattern = Pattern.compile("(^[0-9][0-9].[0-9][0-9]$)|(^[0-9].[0-9]$)|(^[0-9].[0-9][0-9]$)|(^[0-9][0-9].[0-9]$)|(^[0-9]$)|(^.[0-9]$)");
+	
+	UnaryOperator<TextFormatter.Change> filter = c -> {
+		String heure = c.getControlNewText();
+		if (pattern.matcher(heure).matches()) {
+			System.out.println(c);
+			return c;
+		} else {
+			return null;
+		}
+	};
+	
+	StringConverter<Double> converter = new StringConverter<Double>() {
+
+	    @Override
+	    public Double fromString(String s) {
+	        if (s.isEmpty() || "-".equals(s) || ".".equals(s) || "-.".equals(s)) {
+	            return 0.0 ;
+	        } else {
+	            return Double.valueOf(s);
+	        }
+	    }
+
+
+	    @Override
+	    public String toString(Double d) {
+	        return d.toString();
+	    }
+	};
 
 	// Initialisation du Controller
 
@@ -50,8 +85,23 @@ public class ControllerRaidForm {
 		textFieldId.textProperty().bindBidirectional( courant.idProperty(), new IntegerStringConverter()  );
 		textFieldNom.textProperty().bindBidirectional( courant.nomProperty() );
 		UtilFX.bindBidirectional( datePickerAnnee.getEditor(), courant.anneeProperty(), new ConverterStringLocalDate() );
+		if ( courant.getPrix_insc()!=null)
+		{
 		textFieldPrixI.textProperty().bindBidirectional( courant.prix_inscProperty(), new DoubleStringConverter() );
+		TextFormatter<Double> tfD = new TextFormatter<>(filter);
+		textFieldPrixI.setTextFormatter(tfD);
 		textFieldPrixR.textProperty().bindBidirectional( courant.prix_repasProperty(), new DoubleStringConverter() );
+		TextFormatter<Double> tfD2 = new TextFormatter<>(filter);
+		textFieldPrixR.setTextFormatter(tfD2);
+		}else {
+			textFieldPrixI.textProperty().bindBidirectional( courant.prix_inscProperty(), new DoubleStringConverter() );
+			TextFormatter<Double> tfD = new TextFormatter<>(converter, 0.0,filter);
+			textFieldPrixI.setTextFormatter(tfD);
+			textFieldPrixR.textProperty().bindBidirectional( courant.prix_repasProperty(), new DoubleStringConverter() );
+			TextFormatter<Double> tfD2 = new TextFormatter<>(converter, 0.0,filter );
+			textFieldPrixR.setTextFormatter(tfD2);
+			
+		}
 		
 	
 	}
