af9e9611438c8408571f6d707ec5d653ff8318f0
sofianefarkhani <farkhans@3il.fr>
2020-05-25 17:04:34 +0200

    Challenge

--- src/projet/view/poste/ControllerPosteForm.java
+++ src/projet/view/poste/ControllerPosteForm.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package projet.view.poste;
 
 import javax.inject.Inject;
@@ -80,67 +79,3 @@ public class ControllerPosteForm {
 	}
 
 }
-=======
-package projet.view.poste;
-
-import javax.inject.Inject;
-
-
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.util.converter.IntegerStringConverter;
-import jfox.javafx.view.IManagerGui;
-import projet.data.Poste;
-import projet.view.EnumView;
-
-
-public class ControllerPosteForm {
-
-	
-	// Composants de la vue
-	
-	@FXML
-	private TextField		textFieldIdPoste;
-	@FXML
-	private TextField		textFieldIdRaid;
-	@FXML
-	private TextField		textFieldNbrBen;
-	
-	// Autres champs
-	
-	@Inject
-	private IManagerGui		managerGui;
-	@Inject
-	private ModelPoste modelposte;
-
-
-	// Initialisation du Controller
-
-	@FXML
-	private void initialize() {
-
-		// Data binding
-		
-		Poste courant = modelposte.getCourant();
-		textFieldIdPoste.textProperty().bindBidirectional(courant.id_posteProperty(), new IntegerStringConverter());
-		textFieldIdRaid.textProperty().bindBidirectional(courant.id_raidProperty(), new IntegerStringConverter());
-		textFieldNbrBen.textProperty().bindBidirectional(courant.nbr_benevProperty(), new IntegerStringConverter());
-	
-	}
-	
-	
-	// Actions
-	
-	@FXML
-	private void doAnnuler() {
-		managerGui.showView( EnumView.PosteListe );
-	}
-	
-	@FXML
-	private void doValider() {
-		modelposte.validerMiseAJour();
-		managerGui.showView( EnumView.PosteListe );
-	}
-	
-}
->>>>>>> branch 'master' of https://github.com/g11-sav-master/projet
