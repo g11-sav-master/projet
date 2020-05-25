ecb907fcb440be432cd400a53d792985f64133f4
Romain V. <vilayler@3il.fr>
2020-05-25 17:16:03 +0200

    merge

--- src/projet/view/EnumView.java
+++ src/projet/view/EnumView.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package projet.view;
 
 import javafx.scene.Scene;
@@ -10,6 +9,7 @@ public enum EnumView implements IEnumView {
 	
 	// Valeurs
 	
+	Accueil				( "accueil/ViewAccueilListe.fxml" ),
 	Info				( "systeme/ViewInfo.fxml" ),
 	Connexion			( "systeme/ViewConnexion.fxml" ),
 	BenevoleListe		( "benevole/ViewBenevoleListe.fxml" ),
@@ -87,92 +87,3 @@ public enum EnumView implements IEnumView {
 	}
 
 }
-=======
-package projet.view;
-
-import javafx.scene.Scene;
-import jfox.javafx.view.IEnumView;
-
-
-public enum EnumView implements IEnumView {
-
-	
-	// Valeurs
-	
-	Accueil				( "accueil/ViewAccueilListe.fxml" ),
-	Info				( "systeme/ViewInfo.fxml" ),
-	Connexion			( "systeme/ViewConnexion.fxml" ),
-	BenevoleListe		( "benevole/ViewBenevoleListe.fxml" ),
-	BenevoleForm		( "benevole/ViewBenevoleForm.fxml" ),
-	BenevoleListeRole	( "benevole/ViewBenevoleListeRole.fxml" ),
-	BenevoleRoleForm	( "benevole/ViewBenevoleRoleForm.fxml" ),
-	ParticipantListe	( "participant/ViewParticipantListe.fxml" ),
-	ParticipantForm		( "participant/ViewParticipantForm.fxml"),
-	RaidView			( "raid/ViewRaidListe.fxml" ),
-	RaidForm			( "raid/ViewFormRaid.fxml"),
-	PosteListe			( "poste/ViewPosteListe.fxml" ),
-	PosteForm			( "poste/ViewPosteForm.fxml "),
-	CategorieRaidForm	("categorie_raid/ViewCategorieRaidForm.fxml"),
-	CategorieRaidListe	("categorie_raid/ViewCategorieRaidListe.fxml"),
-	ActionBenevoleForm  ("action_benevole/ViewFormActionBenevole.fxml"),
-	ActionBenevoleListe ("action_benevole/ViewListeActionBenevole.fxml"),
-	TestDaoUtilisateur	( "test/ViewTestDaoUtilisateur.fxml" ),
-	TestDaoBenevole		( "test/ViewTestDaoBenevole.fxml" ),
-	TestDaoParticipant	( "test/ViewTestDaoParticipant.fxml" ),
-	TestDaoRaid			( "test/ViewTestDaoRaid.fxml"),
-	TestDaoRoleBenevole ("test/ViewTestDaoRoleBenevole.fxml"),
-	TestDaoValidationMedicale ("test/ViewTestDaoValidation.fxml"),
-	TestDaoCategorieRaid ( "test/ViewTestDaoCategorieRaid.fxml" ),
-	TestDaoParticipantDuo ("test/ViewTestDaoParticipantDuo.fxml"),
-	TestDaoFormeDuo		( "test/ViewTestDaoFormeDuo.fxml" ),
-	TestDaoParticipeOrganisation ( "test/ViewTestDaoParticipeOrganisation.fxml" ),
-	TestDaoPoste 		("test/ViewTestDaoPoste.fxml"),
-	TestDaoActionBenevole ("test/ViewTestDaoActionBenevole.fxml"),
-	EtatPersonnesParCateogire1	( "personne/ViewEtatPersonnesParCategorie1.fxml" ),
-	EtatPersonnesParCateogire2	( "personne/ViewEtatPersonnesParCategorie2.fxml" ),
-	;
-
-	
-	// Champs
-	
-	private String		path;
-	private Object		controller;
-	private Scene		scene;
-
-	
-	// Constructeur 
-	
-	EnumView( String path ) {
-		this.path = path;
-	}
-
-	
-	// Getters & setters
-
-	@Override
-	public String getPath() {
-		return path;
-	}
-	
-	@Override
-	public Object getController() {
-		return controller;
-	}
-
-	@Override
-	public void setController(Object controller) {
-		this.controller = controller;
-	}
-	
-	@Override
-	public Scene getScene() {
-		return scene;
-	}
-	
-	@Override
-	public void setScene(Scene scene) {
-		this.scene = scene;
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/g11-sav-master/projet.git
--- src/projet/view/MenuBarAppli.java
+++ src/projet/view/MenuBarAppli.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package projet.view;
 
 import javax.annotation.PostConstruct;
@@ -76,218 +75,6 @@ public class MenuBarAppli extends MenuBar {
 		this.getMenus().add(menu);
 		menuDonnees = menu;
 		
-		item = new MenuItem( "Bénévoles" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.BenevoleListe )  );
-		menu.getItems().add( item );
-		itemBenevole = item;
-		
-		item = new MenuItem( "Participant" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.ParticipantListe )  );
-		menu.getItems().add( item );
-		itemParticipant = item;
-		
-		item = new MenuItem( "Poste" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.PosteListe )  );
-		menu.getItems().add( item );
-		itemPoste = item;
-		
-		item = new MenuItem( "Raid" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.RaidView )  );
-		menu.getItems().add( item );
-		itemRaid = item;
-		
-		item = new MenuItem( "CategorieRaid" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.CategorieRaidListe )  );
-		menu.getItems().add( item );
-		itemCategorieRaid = item;
-		
-		item = new MenuItem( "Actions Bénévole" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.ActionBenevoleListe )  );
-		menu.getItems().add( item );
-		itemActionBenevole = item;
-		
-		item = new MenuItem( "Validation Médicale" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.ValidationListe )  );
-		menu.getItems().add( item );
-		itemValidationMedicale = item;
-
-		
-		// Menu Tests
-		
-		menu =  new Menu( "Tests" );;
-		this.getMenus().add(menu);
-		menuTests = menu;
-		
-		item = new MenuItem( "DaoUtilisateur" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoUtilisateur )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoBenevole" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoBenevole )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoParticipant" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoParticipant )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoRaid" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoRaid )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoRoleBenevole" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoRoleBenevole )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoValidationMedicale" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoValidationMedicale )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoCategorieRaid" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoCategorieRaid )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoParticipantDuo" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoParticipantDuo )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoFormeDuo" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoFormeDuo )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoParticipeOrganisation" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoParticipeOrganisation )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoPoste" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoPoste )  );
-		menu.getItems().add( item );
-		
-		item = new MenuItem( "DaoActionBenevole" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.TestDaoActionBenevole )  );
-		menu.getItems().add( item );
-
-
-		// Configuration initiale du menu
-		configurerMenu( modelConnexion.getCompteActif() );
-
-		// Le changement du compte connecté modifie automatiquement le menu
-		modelConnexion.compteActifProperty().addListener( (obs) -> {
-					Platform.runLater( () -> configurerMenu( modelConnexion.getCompteActif() ) );
-				}
-			); 
-		
-	}
-
-	
-	// Méthodes auxiliaires
-	
-	private void configurerMenu( Compte compteActif  ) {
-
-		itemDeconnecter.setDisable(true);
-		
-		menuDonnees.setVisible(false);
-		itemBenevole.setVisible(false);
-		itemParticipant.setVisible(false);
-		itemCategorieRaid.setVisible(false);
-		itemPoste.setVisible(false);
-		itemRaid.setVisible(false);
-		itemActionBenevole.setVisible(false);
-		menuTests.setVisible(false);
-		
-		
-		if( compteActif != null ) {
-			itemDeconnecter.setDisable(false);
-			if( compteActif.isInRole( Roles.UTILISATEUR) ) {
-				menuDonnees.setVisible(true);
-			}
-			if( compteActif.isInRole( Roles.ADMINISTRATEUR ) ) {
-				menuDonnees.setVisible(true);
-				itemBenevole.setVisible(true);
-				itemParticipant.setVisible(true);
-				itemPoste.setVisible(true);
-				itemCategorieRaid.setVisible(true);
-				itemRaid.setVisible(true);
-				itemActionBenevole.setVisible(true);
-				menuTests.setVisible(true);
-			}
-		}
-	}
-=======
-package projet.view;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-
-import javafx.application.Platform;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import jfox.javafx.view.IManagerGui;
-import projet.commun.Roles;
-import projet.data.Compte;
-import projet.view.systeme.ModelConnexion;
-
-
-public class MenuBarAppli extends MenuBar {
-
-	
-	// Champs
-	
-	private Menu	menuDonnees;
-	private Menu	menuTests;
-	
-	private MenuItem itemDeconnecter;
-
-	private MenuItem itemBenevole;
-	
-	private MenuItem itemParticipant;
-	
-	private MenuItem itemPoste;
-	
-	private MenuItem itemCategorieRaid;
-	
-	private MenuItem itemRaid;
-	
-	private MenuItem itemActionBenevole;
-	
-	@Inject
-	private IManagerGui 	managerGui;
-	@Inject
-	private ModelConnexion	modelConnexion;	
-	
-	
-	// Initialisation
-	
-	@PostConstruct
-	public void init() {
-
-		
-		// Variables de travail
-		Menu menu;
-		MenuItem item;
-		
-		
-		// Manu Système
-		
-		menu =  new Menu( "Système" );;
-		this.getMenus().add(menu);
-		
-		item = new MenuItem( "Se déconnecter" );
-		item.setOnAction(  (e) -> managerGui.showView( EnumView.Connexion )  );
-		menu.getItems().add( item );
-		itemDeconnecter = item;
-		
-		item = new MenuItem( "Quitter" );
-		item.setOnAction(  (e) -> managerGui.exit()  );
-		menu.getItems().add( item );
-
-		
-		// Manu Données
-		
-		menu =  new Menu( "Données" );;
-		this.getMenus().add(menu);
-		menuDonnees = menu;
-		
 		item = new MenuItem( "Accueil" );
 		item.setOnAction(  (e) -> managerGui.showView( EnumView.Accueil )  );
 		menu.getItems().add( item );
@@ -323,7 +110,10 @@ public class MenuBarAppli extends MenuBar {
 		menu.getItems().add( item );
 		itemActionBenevole = item;
 
-		
+		item = new MenuItem( "Validation Médicale" );
+		item.setOnAction(  (e) -> managerGui.showView( EnumView.ValidationListe )  );
+		menu.getItems().add( item );
+		itemValidationMedicale = item;
 		// Menu Tests
 		
 		menu =  new Menu( "Tests" );;
@@ -424,5 +214,4 @@ public class MenuBarAppli extends MenuBar {
 			}
 		}
 	}
->>>>>>> branch 'master' of https://github.com/g11-sav-master/projet.git
 }
